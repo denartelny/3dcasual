0f8d970b9e4533e209e33bdfacb82918c62e7e3f
Evgeny Artelny <eartelny@gmail.com>
2020-12-05 21:12:33 +0300

    initial version, two level works fine, switch to third doesnt work

---
+++ Assets/Scripts/BlockScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript : MonoBehaviour
+{
+    public float MoveSpeed = 10f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-Vector3.right * MoveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * MoveSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float MoveSpeed = 10f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-Vector3.forward * MoveSpeed * Time.deltaTime);
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Finish")
+        {
+            SceneManager.LoadScene("LVL2");
+        }
+        if (other.gameObject.tag == "Finish2")
+        {
+            SceneManager.LoadScene("LVL3");
+        }
+        if (other.gameObject.tag == "Finish3")
+        {
+            SceneManager.LoadScene("LVL4");
+        }
+        if (other.gameObject.tag == "Finish4")
+        {
+            SceneManager.LoadScene("LVL5");
+        }
+    }
+
+
+
+}
